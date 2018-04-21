77f5322d3bf23c4055c7f3bd88324a084c93adf1
Higher Stark <higherwashington@gmail.com>
2018-04-21 20:14:54 +0800

    A very simple auth implemented

--- src/main/java/com/stark/wordladder/WLController.java
+++ src/main/java/com/stark/wordladder/WLController.java
@@ -1,12 +1,15 @@
 package com.stark.wordladder;
 
+import org.springframework.boot.autoconfigure.web.servlet.error.BasicErrorController;
+import org.springframework.boot.web.servlet.server.Session;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import org.apache.log4j.Logger;
 import org.springframework.web.servlet.ModelAndView;
-
+// import org.springframework.boot.web.servlet.server.Session;
+import javax.servlet.http.HttpServletRequest;
 import java.io.IOException;
 import java.time.LocalDate;
 
@@ -32,8 +35,18 @@ public class WLController {
     @ResponseBody
     @RequestMapping(value="/wordladder", method=RequestMethod.GET)
     public ModelAndView serve (@RequestParam(value="beg", defaultValue = "welcome") String beg,
-                        @RequestParam(value="end", defaultValue = "welcome") String end)
+                        @RequestParam(value="end", defaultValue = "welcome") String end,
+                               HttpServletRequest request)
     {
+        javax.servlet.http.HttpSession session = request.getSession();
+        if (session == null || session.getAttribute("username") == null) {
+            ModelAndView mav = new ModelAndView();
+            mav.setViewName("redirect:/errorpage");
+            mav.addObject("err_msg", "You haven't login yet. Please login");
+            logger.warn("An unauthorized Request - Rejected");
+            return mav;
+        }
+
         ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
         WordLadderServiceIntf wlService = (WordLadderServiceIntf) context.getBean("service");
 
@@ -58,21 +71,32 @@ public class WLController {
      */
     @RequestMapping(value="/login", method=RequestMethod.POST)
     public ModelAndView auth (@RequestParam(value="username", defaultValue="") String userName,
-                      @RequestParam(value="pwd", defaultValue = "") String passwd)
+                      @RequestParam(value="pwd", defaultValue = "") String passwd, HttpServletRequest request)
     {
         logger.info("User: " + userName + " attempts to login with password: " + passwd);
         ModelAndView mav = new ModelAndView();
 
         if (userName.equals("admin") && passwd.equals("123456")) {
             logger.info("Auth success.");
+            javax.servlet.http.HttpSession session = request.getSession();
+            session.setAttribute("username", userName);
             mav.setViewName("redirect:/wordladder");
             // return mav;
         }
         else {
             logger.warn("Auth failed.");
             mav.setViewName("index");
-            mav.addObject("greet", "Welcome to Hotel California~");
+            mav.addObject("greet", "Invalid username or password!");
         }
+        return mav;
+    }
+
+    @RequestMapping(value="/errorpage", method=RequestMethod.GET)
+    public ModelAndView error(@RequestParam(value="err_msg", defaultValue = "Welcome to Hotel California") String err_msg)
+    {
+        ModelAndView mav = new ModelAndView();
+        mav.setViewName("/errorpage");
+        mav.addObject("err_msg", err_msg);
         return mav;
     }
 }
